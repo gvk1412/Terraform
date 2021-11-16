2392a8e3dccac54a60196198e3643c3d5e765a32
vamsi <oracle0208@gmail.com>
2021-11-16 20:58:38 +0530

    terraform infra build code

--- main.tf
+++ main.tf
@@ -10,4 +10,15 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "ap-south-1"
+  access_key = "AKIAX37KI4XVDF3P4LNU"
+  secret_key = "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"
+
 }
+resource "aws_instance" "terraform-1" {
+  ami= "ami-0f1fb91a596abf28d"
+  instance_type = "t3.micro"
+  tags={
+      Name="tf-1"
+  }  
+}
+
